d6f00fd60db35e9748e53895c8521e3514d8ece3
alastairmoody-madeye <akshata.lakshmi@gmail.com>
2021-02-23 18:45:49 +0530

    Convert exponential to float

--- hello.pas
+++ hello.pas
@@ -1,18 +1,22 @@
 program fcalc;
 uses crt;
 const
-PI=3.14;
+PI=314e-2;
 var
-num, i, sum: nteger;
-condition: boolea;
+num, i, sum: integer;
+condition: boolean;
+human_name: string;
 begin
 num:=5;
 sum:=0;
+human_name:='isima';
 for i:=1 to num do
 begin
     sum := sum + i;
 end;
 condition := true;
 if (condition = true) then 
-    writeln("TRUE BLOCK");
+    writeln('"Hello"');
+else
+    writeln('Bye');
 end.
