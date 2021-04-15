2461b2c831e64aa2426b30d41d9b4109ceac177b
ishi20 <iagarwal20@gmail.com>
2021-04-15 18:06:39 +0530

    Checking for types in assignment statements

--- examples/working1.pas
+++ examples/working1.pas
@@ -3,23 +3,29 @@ const
 PI = 3.141592654;
 (* This is pi, the value which is c/d for a circle *)
 var
-r, d : integer;   {variable declaration: radius, dia, circumference}
-c : real;
+r: integer;   {variable declaration: radius, dia, circumference}
+c , d: real;
 color: integer;
+e: boolean;
 
 begin
-   r := 10;
+   {r := 's';}
+   r := 0;
    d := 4;
    color := 1;
-   c := 1;
+   c := 1.0;
+   e := False;
    if color = 1 then
       if color = 2 then
             c := c + 1
          else
             c := c + 2
    else
-      c := c + 3;        
-   d := 2 * r;
-   c :=  PI * d;
+   c := c + 3;        
+   d := r * 2;
+   {r :=  PI * d;}
+   {e := e + 0;}
+   writeln('The circumference of the circle is ',c);
+   c := 2.0;
    writeln('The circumference of the circle is ',c);
 end.
