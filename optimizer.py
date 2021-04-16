8dc640e9af09d31853f8475dbc16ca3e85ad1e82
alastairmoody-madeye <akshata.lakshmi@gmail.com>
2021-04-16 19:08:11 +0530

    Add common expression elim, constant folding and constant prop

--- optimizer.py
+++ optimizer.py
@@ -7,12 +7,13 @@
 import copy
 import sys
 import re
+from collections import namedtuple
 
 
 def is_real_num(number):
     if number.isnumeric():
         return True
-    elif re.match(r"\d+\.*\d*", number) pr re.match(r"-\d+\.*\d*", number):
+    elif re.match(r"\d+\.*\d*", number) or re.match(r"-\d+\.*\d*", number):
         return True
     else:
         return False
@@ -116,10 +117,10 @@ class TACMod(object):
 class TACAssign(object):
     def __init__(self, op1, result):
         self.op = '='
-        sefl.args = []
+        self.args = []
         self.args.append(op1)
         self.result = result
-        self.is_const = is_real_num(self.op1)
+        self.is_const = is_real_num(op1)
     def __str__(self):
         string_to_ret = str(self.op) + ' '
         for i in self.args:
@@ -128,13 +129,205 @@ class TACAssign(object):
         return string_to_ret
 
 class TACFun(object):
-    def __init__(self, label, args):
+    def __init__(self, label, args, result):
         self.label = label
+        self.op = 'call'
+        self.result = result
         self.args = args
     def __str__(self):
-        string_to_ret = str(self.label) + ' '
+        string_to_ret = str(self.result) + ' = ' + str(self.label) + '('
+        for i in range(len(self.args)-1):
+            string_to_ret += str(self.args[i]) + ', '
+        string_to_ret += self.args[-1] + ')'
+        return string_to_ret
+
+class TACEquals(object):
+    def __init__(self, op1, op2, result):
+        self.op = '=='
+        self.args = []
+        self.args.append(op1)
+        self.args.append(op2)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
         for i in self.args:
             string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACNEquals(object):
+    def __init__(self, op1, op2, result):
+        self.op = '!='
+        self.args = []
+        self.args.append(op1)
+        self.args.append(op2)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACLEquals(object):
+    def __init__(self, op1, op2, result):
+        self.op = '<='
+        self.args = []
+        self.args.append(op1)
+        self.args.append(op2)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACGEquals(object):
+    def __init__(self, op1, op2, result):
+        self.op = '>='
+        self.args = []
+        self.args.append(op1)
+        self.args.append(op2)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACGThan(object):
+    def __init__(self, op1, op2, result):
+        self.op = '>'
+        self.args = []
+        self.args.append(op1)
+        self.args.append(op2)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACLThan(object):
+    def __init__(self, op1, op2, result):
+        self.op = '<'
+        self.args = []
+        self.args.append(op1)
+        self.args.append(op2)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACLAnd(object):
+    def __init__(self, op1, op2, result):
+        self.op = '&&'
+        self.args = []
+        self.args.append(op1)
+        self.args.append(op2)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACLOr(object):
+    def __init__(self, op1, op2, result):
+        self.op = '||'
+        self.args = []
+        self.args.append(op1)
+        self.args.append(op2)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACBAnd(object):
+    def __init__(self, op1, op2, result):
+        self.op = '&'
+        self.args = []
+        self.args.append(op1)
+        self.args.append(op2)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACBOr(object):
+    def __init__(self, op1, op2, result):
+        self.op = '|'
+        self.args = []
+        self.args.append(op1)
+        self.args.append(op2)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACBXor(object):
+    def __init__(self, op1, op2, result):
+        self.op = '^'
+        self.args = []
+        self.args.append(op1)
+        self.args.append(op2)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACBNot(object):
+    def __init__(self, op, result):
+        self.op = '~'
+        self.args = []
+        self.args.append(op)
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(self.result)
+        return string_to_ret
+
+class TACConst(object):
+    def __init__(self, value, result):
+        self.op = 'const'
+        self.value = value
+        self.result = result
+        self.args = []
+    def __str__(self):
+        string_to_ret = str(self.op) + ' ' + str(self.value) + ' ' + str(self.result)
+        return string_to_ret
+
+class TACId(object):
+    def __init__(self, args, result):
+        self.op = 'id'
+        self.args = [args]
+        self.result = result
+    def __str__(self):
+        string_to_ret = str(self.op) + ' '
+        for i in self.args:
+            string_to_ret += str(i) + ' '
+        string_to_ret += str(result)
         return string_to_ret
 
 class TACBasicBlock(object):
@@ -201,9 +394,19 @@ def make_bbs(inst):
 
 def create_CFG(code):
     inst_list = []
+    param_list = []
     for tacinst in code:
         inst = tacinst.split(',')
-        if inst[0] == 'Label':
+        if inst[0] == 'call':
+            n = int(inst[2].strip())
+            label = inst[1].strip()
+            result = inst[3].strip()
+            args = [param_list[i] for i in range(len(param_list)-n, len(param_list))]
+            inst_list.append(TACFun(label, args, result))
+            param_list = param_list[:len(param_list)-n]
+        elif inst[0] == 'param':
+            param_list.append(inst[1].strip())
+        elif inst[0] == 'label':
             label = inst[-1].strip()
             inst_list.append(TACLabel(label))
         elif inst[0] == 'goto':
@@ -242,14 +445,72 @@ def create_CFG(code):
                 op1 = inst[1].strip()
                 op2 = inst[2].strip()
                 inst_list.append(TACMod(op1, op2, assignee))
+            elif inst[0] == '&&':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                op2 = inst[2].strip()
+                inst_list.append(TACLAnd(op1, op2, assignee))
+            elif inst[0] == '||':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                op2 = inst[2].strip()
+                inst_list.append(TACLOr(op1, op2, assignee))
+            elif inst[0] == '&':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                op2 = inst[2].strip()
+                inst_list.append(TACBAnd(op1, op2, assignee))
+            elif inst[0] == '|':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                op2 = inst[2].strip()
+                inst_list.append(TACBOr(op1, op2, assignee))
+            elif inst[0] == '^':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                op2 = inst[2].strip()
+                inst_list.append(TACBXor(op1, op2, assignee))
+            elif inst[0] == '~':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                inst_list.append(TACBNot(op1, assignee))
             elif inst[0] == '=':
                 assignee = inst[3].strip()
                 op = inst[1].strip()
                 inst_list.append(TACAssign(op, assignee))
+            elif inst[0] == '==':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                op2 = inst[2].strip()
+                inst_list.append(TACEquals(op1, op2, assignee))
+            elif inst[0] == '!=':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                op2 = inst[2].strip()
+                inst_list.append(TACNEquals(op1, op2, assignee))
+            elif inst[0] == '>=':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                op2 = inst[2].strip()
+                inst_list.append(TACGEquals(op1, op2, assignee))
+            elif inst[0] == '<=':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                op2 = inst[2].strip()
+                inst_list.append(TACLEquals(op1, op2, assignee))
+            elif inst[0] == '>':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                op2 = inst[2].strip()
+                inst_list.append(TACGThan(op1, op2, assignee))
+            elif inst[0] == '<':
+                assignee = inst[3].strip()
+                op1 = inst[1].strip()
+                op2 = inst[2].strip()
+                inst_list.append(TACLThan(op1, op2, assignee))
         else:
-            label = inst[0].strip()
-            args = [ins.strip() for ins in inst[1:]]
-            inst_list.append(TACFun(label, args))
+            print("Illegal instruction.")
+            exit(1)
     # global dead code elimination before makinig it into basic blocks
     instructions = global_dead_code_elimination(inst_list)
     basic_blocks = make_bbs(instructions)
@@ -269,8 +530,12 @@ def basic_dead_code_elimination(code):
             for i in instr.args:
                 used.add(i)
     for instr in code:
-        if hasattr(instr, 'result') and instr.result not in used:
-            code.remove(instr)
+        if hasattr(instr, 'result'):
+            if instr.op == 'call':
+                if instr.result not in used and len(used.intersection(set(instr.args))) == 0:
+                    code.remove(instr)
+            elif instr.result not in used:
+                code.remove(instr)
     return code
 
 
@@ -370,6 +635,9 @@ def last_writes(block):
     return last_write
 
 
+Value = namedtuple('Value', ['op', 'args'])
+
+
 def local_value_numbering_block(block):
     # main table. Update each time a variable is modified
     var2num = Numbering()
@@ -390,7 +658,111 @@ def local_value_numbering_block(block):
             num2var[num] = var
         
         for instr, last_write in zip(block.instructions, last_writes(block)):
+            argvars = instr.args
+            argnums = tuple(var2num[var] for var in argvars)
+
+            val = None
+            if hasattr(instr, 'result') and hasattr(instr, 'args') and not isinstance(instr, TACFun):
+                val = canonicalize(Value(instr.op, argnums))
+
+                num = lookup(value2num, val)
+                if num is not None:
+                    var2num[instr.result] = num
+                    
+                    # constant propagation
+                    if num in num2const:
+                        new_instr = TACConst(value, instr.result)
+                        instr = new_instr
+
+                    else:
+                        new_instr = TACId(num2var[num], instr.result)
+                        instr = new_instr
+                        # change this part also
+                        instr.op = 'id'
+                        instr.args = [num2var[num]]
+                    continue
             
+            if hasattr(instr, 'result'):
+                newnum = var2num.add(instr.result)
+
+                if instr.op == 'const':
+                    num2const[newnum] = instr.value
+                if last_write:
+                    var = instr.result
+                else:
+                    var = 'lvn.{}'.format(newnum)
+                    # in case we use that variable again, use updated var
+                
+                num2var[newnum] = var
+
+                instr.result = var
+
+                if val is not None:
+                    # constant folding
+                    const = fold(num2const, val)
+                    if const is not None:
+                        num2const[newnum] = const
+                        # change this part too
+                        new_instr = TACConst(const, instr.result)
+                        instr = new_instr
+                        continue
+                    value2num[val] = newnum
+
+            if hasattr(instr, 'args'):
+                instr.args = [num2var[n] for n in argnums]
+
+
+def lookup(value2num, val):
+    if val.op == 'id':
+        return val.args[0]
+    else:
+        return value2num.get(val)
+
+
+FOLDABLE_OPS = {
+    '+': lambda a, b: a + b,
+    '*': lambda a, b: a * b,
+    '-': lambda a, b: a - b,
+    '/': lambda a, b: a / b,
+    '>': lambda a, b: a > b,
+    '<': lambda a, b: a < b,
+    '>=': lambda a, b: a >= b,
+    '<=': lambda a, b: a <= b,
+    '!=': lambda a, b: a != b,
+    '==': lambda a, b: a == b,
+    '||': lambda a, b: a or b,
+    '&&': lambda a, b: a and b,
+    '~': lambda a: not a,
+    '&': lambda a, b: a & b,
+    '|': lambda a, b: a | b,
+    '^': lambda a, b: a ^ b
+}
+
+
+def fold(num2const, val):
+    if val.op in FOLDABLE_OPS:
+        try:
+            const_args = [num2const[n] for n in val.args]
+            return FOLDABLE_OPS[value.op](*const_args)
+        except KeyError:    # at least one arg is not a constant
+            if val.op in {'==', '!=', '<=', '>='} and val.args[0] == val.args[1]:
+                return val.op != '!='
+            if val.op in {'&&', '||'} and any(v in num2const for v in val.args):
+                const_val = num2const[val.args[0] if val.args[0] in num2const else val.args[1]]
+                if(val.op == '&&' and not const_val) or (val.op == '||' and const_val):
+                    return const_val
+            return None
+        except ZeroDivisionError:
+            return None
+    else:
+        return None
+
+
+def canonicalize(value):
+    if value.op in ('+', '*'):
+        return Value(value.op, tuple(sorted(value.args)))
+    else:
+        return value
 
 
 def local_value_numbering(basic_blocks):
@@ -417,19 +789,7 @@ def local_value_numbering(basic_blocks):
 
     for bb in basic_blocks:
         local_value_numbering_block(bb)
-    return basic_block
-
-
-def common_sub_exp_elimination(code):
-    return code
-
-
-def constant_folding(code):
-    return code
-
-
-def constant_propogation(code):
-    return code
+    return basic_blocks
 
 
 def loop_opt(code):
@@ -438,9 +798,8 @@ def loop_opt(code):
 
 def optimize(code):
     code = dead_code_elimination(code)
-    code = common_sub_exp_elimination(code)
-    code = constant_folding(code)
-    code = constant_propogation(code)
+    # LVN performs common subexpression elimination, constant folding and constant propagation
+    code = local_value_numbering(code)
     code = loop_opt(code)
     with open("opt_code.out", "w") as file:
         sys.stdout = file
