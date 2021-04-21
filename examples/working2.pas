687796c72a592e3744e8e768f57f1cb1c83ba885
ishi20 <iagarwal20@gmail.com>
2021-04-22 00:52:06 +0530

    Example

--- examples/working2.pas
+++ examples/working2.pas
@@ -1,14 +1,31 @@
-program forLoop;
-
+program const_circle;
+const
+PI = 3.141592654;
+(* This is pi, the value which is c/d for a circle *)
 var
-a: integer;
-b,c: string;
+r: integer;   {variable declaration: radius, dia, circumference}
+c , d: real;
+color: integer;
+e: boolean;
 
 begin
-   for a := 10  to 20 do
-   begin
-    if a = 20 then
-      writeln('value of a: ', a);
-   end;
-    c:="Mango";
+   {r := 's';}
+   r := 0;
+   d := 4;
+   color := 1;
+   c := 1.0;
+   e := False;
+   if color = 1 then
+      if color = 2 then
+            c := c + 1
+         else
+            c := c + 2
+   else
+      c := c + 3;        
+   d := r * 2;
+   {r :=  PI * d;}
+   {e := e + 0;}
+   writeln('The circumference of the circle is ',c);
+   c := 2.0;
+   writeln('The circumference of the circle is ',c);
 end.
